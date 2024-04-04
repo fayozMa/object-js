21130df6477ed26659f4f9f992b3aa34f444c2f3
Muhammadfayyozbek Ahmadjonov <usmonovbahriddin108@gmail.com>
2024-04-04 15:59:54 +0500

    [ADD] first homework

---
+++ index1.js
@@ -0,0 +1,10 @@
+const student = {
+	name : "David Rayy",
+	sclass : "VI",
+	rollno : 12 ,
+    func : () => {
+        console.log(Object.keys(student))
+    } 
+}
+student.func()
+// result: name, sclass, rollno
